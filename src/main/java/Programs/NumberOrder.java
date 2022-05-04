6a497e0d7d6ac0b0b919e2b8c869cab7d7e9ea6a
shivanivathsavai <shivanivathsavai@gmail.com>
2022-05-04 10:25:03 +0530

    commit

--- src/main/java/Programs/NumberOrder.java
+++ src/main/java/Programs/NumberOrder.java
@@ -7,45 +7,43 @@ import java.util.TreeSet;
 
 public class NumberOrder
 {
-    public static void main(String[] args)
+    public static  void order(int[] input,boolean flag)
     {
-        int[] input = {4, 3, 5, 1, 2};
         int temp;
-        Boolean flag;
-        if (flag = true)
-        {
-            for (int i = 0; i < input.length; i++)
-            {
-                for (int j = i + 1; j < input.length; j++)
-                {
 
-                    if (input[i] > input[j])
-                    {
-                        temp = input[i];
-                        input[i] = input[j];
-                        input[j] = temp;
-                    }
+        for (int i = 0; i < input.length; i++)
+        {
+            for (int j = i + 1; j < input.length; j++)
+            {
+                if (input[i] > input[j])
+                {
+                    temp = input[i];
+                    input[i] = input[j];
+                    input[j] = temp;
                 }
 
             }
-            System.out.println("Ascending order:" + Arrays.toString(input));
+
+        }
+        if (flag )
+        {
+            for(int array:input)
+            {
+                System.out.print( array+",");
+            }
         }
         else
         {
-            for (int i = 0; i < input.length; i++)
+            for (int desc=input.length-1; desc>=0; desc--)
             {
-                for (int j = i + 1; j < input.length; j++)
-                {
-                    if (input[i] < input[j])
-                    {
-                        temp = input[i];
-                        input[i] = input[j];
-                        input[j] = temp;
-
-                    }
-                }
+                System.out.print(input[desc]+",");
             }
-            System.out.println("Descending order:" + Arrays.toString(input));
         }
+    }
+
+    public static void main(String[] args) {
+        int[] input = {4, 3, 5, 1, 2};
+        order(input,false);
+
     }
 }
